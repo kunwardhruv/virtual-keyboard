39bb3eba4914a9a4e582b86f13a17427686d5c50
Dhruv Singh <kunwarrdhruv@gmail.com>
2025-09-19 18:28:07 +0530

    Initial commit - Virtual Keyboard Project

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+
+def drawAll(img, buttonList):
+    for button in buttonList:
+        x, y = button.pos
+        w, h = button.size
+        cv2.rectangle(img, button.pos, (x + w, y + h), (255, 0, 255), cv2.FILLED)
+        cv2.putText(img, button.text, (x + 20, y + 65),
+                    cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+    return img
